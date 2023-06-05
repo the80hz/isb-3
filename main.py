b8b4b033577ffb6f87ba1a12cefde513d47f36fc
the80hz <dnl.vlgv@mail.ru>
2023-06-05 10:31:11 +0400

    файл с настройками задается через параметры запуска

--- main.py
+++ main.py
@@ -12,45 +12,21 @@ lyrics = ['О, горячий суп наварили!\nО, великий су�
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Encrypt and decrypt file using hybrid crypto-system')
+    parser.add_argument('--settings', type=str, help='settings path')
     parser.add_argument('-g', '--generate', action='store_true', help='generate keys')
     parser.add_argument('-e', '--encrypt', action='store_true', help='encrypt file')
     parser.add_argument('-d', '--decrypt', action='store_true', help='decrypt file')
     args = parser.parse_args()
 
-    if not args.generate and not args.encrypt and not args.decrypt:
+    if not args.settings:
         parser.print_help()
         exit()
 
-    if not os.path.exists('settings.json'):
-        settings.get_settings()
-
-    with open('settings.json', 'r') as f:
-        settings = json.load(f)
-
-    if not os.path.exists(settings['initial_file']):
-        try:
-            with open(settings['initial_file'], 'w') as f:
-                f.write('')
-        except FileNotFoundError:
-            print('Something went wrong')
-    if not os.path.exists(settings['output_folder']):
-        try:
-            os.makedirs(settings['output_folder'])
-        except FileNotFoundError:
-            print('Something went wrong')
-
-    if not os.path.exists(settings['output_keys_folder']):
-        try:
-            os.makedirs(settings['output_keys_folder'])
-        except FileNotFoundError:
-            print('Something went wrong')
-
-    if os.path.getsize(settings['initial_file']) == 0:
-        try:
-            with open(settings['initial_file'], 'w', encoding='utf-8') as f:
-                f.write(f'{lyrics[0] * 4}{lyrics[1] * 4}{lyrics[0] * 2}{lyrics[1] * 8}{lyrics[0] * 2}{lyrics[1] * 4}')
-        except FileNotFoundError:
-            print('Something went wrong')
+    try:
+        with open(args.settings, 'r') as f:
+            settings = json.load(f)
+    except Exception as ex:
+        print(ex)
 
     if args.generate:
         generate_keys(settings['symmetric_key'], settings['public_key'],
